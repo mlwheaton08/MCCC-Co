879fda2c51a11e430ea6d2c6b4dda21627fd758b
Mason Wheaton <mlwheaton06@gmail.com>
2023-05-23 21:01:45 -0500

    API setup. added Add and GetByFirebaseId to UserRepository

---
+++ MCCC Co/Repositories/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using MCCC_Co_.Models;
+
+namespace MCCC_Co_.Repositories
+{
+    public interface IUserRepository
+    {
+        User GetByFirebaseId(string firebaseId);
+        void Add(User user);
+    }
+}
---
+++ MCCC Co/Repositories/UserRepository.cs
@@ -0,0 +1,80 @@
+﻿using MCCC_Co_.Models;
+using MCCC_Co_.Utils;
+
+namespace MCCC_Co_.Repositories;
+
+public class UserRepository : BaseRepository, IUserRepository
+{
+    public UserRepository(IConfiguration configuration) : base(configuration) { }
+
+    public User GetByFirebaseId(string firebaseId)
+    {
+        using (var conn = Connection)
+        {
+            conn.Open();
+            using (var cmd = conn.CreateCommand())
+            {
+                cmd.CommandText = @"SELECT
+                                        Id,
+	                                    FirebaseId,
+	                                    IsAdmin,
+	                                    [Name],
+	                                    Email,
+	                                    RewardsPoints
+                                    FROM [User]
+                                    WHERE FirebaseId = @firebaseId";
+                DbUtils.AddParameter(cmd, "@firebaseId", firebaseId);
+
+                var reader = cmd.ExecuteReader();
+
+                User user = null;
+                if (reader.Read())
+                {
+                    user = new User()
+                    {
+                        Id = DbUtils.GetInt(reader, "Id"),
+                        FirebaseId = DbUtils.GetString(reader, "FirebaseId"),
+                        IsAdmin = DbUtils.GetBoolean(reader, "IsAdmin"),
+                        Name = DbUtils.GetString(reader, "Name"),
+                        Email = DbUtils.GetString(reader, "Email"),
+                        RewardsPoints = DbUtils.GetNullableInt(reader, "RewardsPoints")
+                    };
+                }
+
+                reader.Close();
+                return user;
+            }
+        }
+    }
+
+    public void Add(User user)
+    {
+        using (var conn = Connection)
+        {
+            conn.Open();
+            using (var cmd = conn.CreateCommand())
+            {
+                cmd.CommandText = @"INSERT INTO [User]
+	                                    (FirebaseId,
+	                                    IsAdmin,
+	                                    [Name],
+	                                    Email,
+	                                    RewardsPoints)
+                                    OUTPUT INSERTED.ID
+                                    VALUES
+	                                    (@FirebaseId,
+	                                    @IsAdmin,
+	                                    @Name,
+	                                    @Email,
+	                                    @RewardsPoints)";
+                DbUtils.AddParameter(cmd, "@FirebaseId", user.FirebaseId);
+                DbUtils.AddParameter(cmd, "@IsAdmin", user.IsAdmin);
+                DbUtils.AddParameter(cmd, "@Name", user.Name);
+                DbUtils.AddParameter(cmd, "@Email", user.Email);
+                DbUtils.AddParameter(cmd, "@RewardsPoints", user.RewardsPoints);
+
+                user.Id = (int)cmd.ExecuteScalar();
+            }
+        }
+    }
+}
